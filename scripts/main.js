7473062174a68f3536665440ef1912c3a52557e1
Marian Powell <marianpowell@gmail.com>
2016-09-27 17:01:47 -0400

    HTML going to page. unsettled

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,67 @@
 
 You will want to use the split() method of strings to break up the string first. You can use one of Shakespeare's Sonnets as your test case.*/
 
-var testString = "apple pear axe grind. ate ate, lox!\na";
-var contentArea = document.getElementById('content');
+var testString = `THE SONNETS
+
+by William Shakespeare
+
+                     1
+  From fairest creatures we desire increase,
+  That thereby beauty's rose might never die,
+  But as the riper should by time decease,
+  His tender heir might bear his memory:
+  But thou contracted to thine own bright eyes,
+  Feed'st thy light's flame with self-substantial fuel,
+  Making a famine where abundance lies,
+  Thy self thy foe, to thy sweet self too cruel:
+  Thou that art now the world's fresh ornament,
+  And only herald to the gaudy spring,
+  Within thine own bud buriest thy content,
+  And tender churl mak'st waste in niggarding:
+    Pity the world, or else this glutton be,
+    To eat the world's due, by the grave and thee.
+
+
+                     2
+  When forty winters shall besiege thy brow,
+  And dig deep trenches in thy beauty's field,
+  Thy youth's proud livery so gazed on now,
+  Will be a tattered weed of small worth held:
+  Then being asked, where all thy beauty lies,
+  Where all the treasure of thy lusty days;
+  To say within thine own deep sunken eyes,
+  Were an all-eating shame, and thriftless praise.
+  How much more praise deserved thy beauty's use,
+  If thou couldst answer 'This fair child of mine
+  Shall sum my count, and make my old excuse'
+  Proving his beauty by succession thine.
+    This were to be new made when thou art old,
+    And see thy blood warm when thou feel'st it cold.
+
+
+                     3
+  Look in thy glass and tell the face thou viewest,
+  Now is the time that face should form another,
+  Whose fresh repair if now thou not renewest,
+  Thou dost beguile the world, unbless some mother.
+  For where is she so fair whose uneared womb
+  Disdains the tillage of thy husbandry?
+  Or who is he so fond will be the tomb,
+  Of his self-love to stop posterity?
+  Thou art thy mother's glass and she in thee
+  Calls back the lovely April of her prime,
+  So thou through windows of thine age shalt see,
+  Despite of wrinkles this thy golden time.
+    But if thou live remembered not to be,
+    Die single and thine image dies with thee.`;
+
 
 /* this function takes a string and returns an object containing words as the keys and counts of that word as values. */
 function countWords(theString) {
   var arr1 = []; //theString will be broken down and placed here.
   var arr2 = []; //this is where to put processed array of strings.
   var map = {}; //the object that will hold the words and their counts.
-
+var contentArea = document.getElementById('content');
     function cleanUp(){
       /* iterate through string and build an array of words by replacing newlines with spaces, then splitting by spaces. */
       arr1 = theString.replace(/\n/g, " ").split(" ");
@@ -24,7 +76,6 @@ function countWords(theString) {
         }
       }
     }
-
     function addItemsToMapObject() {
       for (var item1 in arr2) {
         var word = arr2[item1];
@@ -37,6 +88,7 @@ function countWords(theString) {
         } else {
           map[word] = 1;
         }
+        contentArea.innerHTML += word + ": " + map[word] + "<br>";
         console.log("word: map[word] = " + word + ": " + map[word]);
       }
     }
