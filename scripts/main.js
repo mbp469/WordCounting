07102d7ad0692d896a741837194b0f68e2de6f72
Marian Powell <marianpowell@gmail.com>
2016-09-27 16:46:51 -0400

    added functions to clean up. still no HTML but console.log is working.

--- scripts/main.js
+++ scripts/main.js
@@ -7,34 +7,28 @@ var contentArea = document.getElementById('content');
 
 /* this function takes a string and returns an object containing words as the keys and counts of that word as values. */
 function countWords(theString) {
+  var arr1 = []; //theString will be broken down and placed here.
+  var arr2 = []; //this is where to put processed array of strings.
+  var map = {}; //the object that will hold the words and their counts.
 
-    /* iterate through string and build an array of words by replacing newlines with spaces, then splitting by spaces. */
-    var arr1 = theString.replace(/\n/g, " ").split(" ");
-
-    console.log("arr1: " + arr1);
-
-    var arr2 = []; //this is where to put processed array of strings.
-
-    var map = {}; //the object that will hold the words and their counts.
-
-    /* go through the array and for each item, test for character-length, revert to lower case, remove punctuation using:[replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")], add acceptable array items to new array */
-
-    for (var item in arr1) {
-        //character.replace punctuation with "" and changes to lower case
-        arr1[item] = arr1[item].replace(/[!.,?'"-]/g,"").toLowerCase();
-        /* push the item to arr2 if more than one character long */
-        if(arr1[item].length > 1) {
-        arr2.push(arr1[item]);
-        console.log("arr1[item]: " + arr1[item]);
+    function cleanUp(){
+      /* iterate through string and build an array of words by replacing newlines with spaces, then splitting by spaces. */
+      arr1 = theString.replace(/\n/g, " ").split(" ");
+      /* go through the array and for each item, test for character-length, revert to lower case, remove punctuation using:[replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")], add acceptable array items to new array */
+      for (var item in arr1) {
+          //character.replace punctuation with "" and changes to lower case
+          arr1[item] = arr1[item].replace(/[!.,?'"-]/g,"").toLowerCase();
+          /* push the item to arr2 if more than one character long */
+          if(arr1[item].length > 1) {
+          arr2.push(arr1[item]); // the cleaned up items are now stored in arr2.
+        }
       }
     }
-      console.log("arr2: " + arr2);
-      console.log("arr2.length: " + arr2.length);
 
-      for (var item1 in arr2) { //loop through the array and save the current item as a word in the object
+    function addItemsToMapObject() {
+      for (var item1 in arr2) {
         var word = arr2[item1];
-        console.log(word);
-        // /* if the map contains the key word, retrieve the property and add one. */
+        /* if the map contains the key word, retrieve the property and add one. */
         if(map[word]) {
           var count = map[word];
           count++;
@@ -45,5 +39,8 @@ function countWords(theString) {
         }
         console.log("word: map[word] = " + word + ": " + map[word]);
       }
+    }
+    cleanUp();
+    addItemsToMapObject();
 } //end of countWords function
 countWords(testString);
